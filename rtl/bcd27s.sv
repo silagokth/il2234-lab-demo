a7ccd48f4a1f19a1b8b130421466bbf999e52a9b
Oliver Theimer <75044467+otheimer@users.noreply.github.com>
2026-08-13 13:31:36 +0200

    Add hex segment mappings to bcd27s.sv

--- rtl/bcd27s.sv
+++ rtl/bcd27s.sv
@@ -18,8 +18,14 @@ module bcd27s (
       4'b0111: seg = 7'b1111000;
       4'b1000: seg = 7'b0000000;
       4'b1001: seg = 7'b0010000;
+      //hex
+      4'b1010: seg = 7'b0001000;
+      4'b1011: seg = 7'b0000011;
+      4'b1100: seg = 7'b1000110;
+      4'b1101: seg = 7'b0100001;
+      4'b1110: seg = 7'b0000110;
+      4'b1111: seg = 7'b0001110;
       default: seg = 7'b1111111;
     endcase
   end
 endmodule
-
